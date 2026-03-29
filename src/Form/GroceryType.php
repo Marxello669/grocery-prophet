770176c9f97a6cfa2985d85aa984992022e4222a
Dário Santos <dariovfsantos@gmail.com>
2026-03-29 11:40:22 +0100

    Updated new grocery form

--- src/Form/GroceryType.php
+++ src/Form/GroceryType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Grocery;
+use App\Enum\GroceryEnum;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +15,14 @@ class GroceryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('type');
+            ->add('name', TextType::class, [
+                'attr' => ['placeholder' => 'Grocery Name']
+            ])
+            ->add('type', EnumType::class, [
+                'class' => GroceryEnum::class,
+                'choice_label' => 'label',
+                'placeholder' => 'Grocery Type'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
